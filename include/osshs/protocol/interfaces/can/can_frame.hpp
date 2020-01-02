a9124db82b0caca09a9f75c4f8e6182d5757f6bf
Linas Nikiperavicius <linas@linasdev.com>
2020-01-03 00:02:33 +0200

    Extend CAN frame class

--- include/osshs/protocol/interfaces/can/can_frame.hpp
+++ include/osshs/protocol/interfaces/can/can_frame.hpp
@@ -25,6 +25,7 @@
 #ifndef OSSHS_PROTOCOL_CAN_FRAME_HPP
 #define OSSHS_PROTOCOL_CAN_FRAME_HPP
 
+#include <modm/platform.hpp>
 #include <memory>
 
 namespace osshs
@@ -40,17 +41,68 @@ namespace osshs
 				public:
 					/**
 					 * @brief Construct CAN frame from parameters and data.
-					 * @param data Data.
+					 * @param data Up to 8 bytes of data (7 bytes if lastFrameId > 0).
+					 * @param dataLen Length of data.
 					 * @param transmitterMac Transmitter device MAC address.
-					 * @param frameCount Total number of frames in current packet.
+					 * @param lastFrameId Last frame id inside current packet.
 					 * @param frameId Current frame id.
 					 * @param error Whether or not this is an error frame.
 					 */
-					CanFrame(std::unique_ptr<const uint8_t[]> data, uint32_t transmitterMac, uint16_t frameCount = 1, uint16_t frameId = 0, bool error = false);
+					CanFrame(const uint8_t *data, uint8_t dataLen, uint16_t transmitterMac,
+						uint16_t lastFrameId = 0, uint16_t frameId = 0, bool error = false);
 
+					/**
+					 * @brief Construct CAN frame from a modm CAN message.
+					 * @param message Modm CAN message.
+					 */
+					CanFrame(modm::can::Message message);
+
+					/**
+					 * @brief Get pointer to data.
+					 * @return Pointer to data.
+					 */
+					const uint8_t *
+					getData();
+
+					/**
+					 * @brief Get data length.
+					 * @return Data length.
+					 */
+					uint8_t
+					getDataLen();
+
+					/**
+					 * @brief Get transmitter device MAC address.
+					 * @return Transmitter device MAC address.
+					 */
+					uint16_t
+					getTransmitterMac();
+
+					/**
+					 * @brief Get current or last frame id.
+					 * @note Returns last frame id if current frame id is zero, otherwise returns current frame id.
+					 * @return Current or last frame id.
+					 */
+					uint16_t
+					getFrameId();
+
+					/**
+					 * @brief Check if this frame is an error frame.
+					 * @return Whether or not this frame is an error frame.
+					 */
+					bool
+					isError();
+
+					/**
+					 * @brief Check if this frame is part of a multi frame packet.
+					 * @return Whether or not this frame is part of a multi frame packet.
+					 */
+					bool
+					isMultiFrame();
 				private:
-					uint32_t extendedIdentifier;
 					std::unique_ptr<const uint8_t[]> data;
+					uint8_t dataLen;
+					uint16_t extendedIdentifier;
 				};
 			}
 		}
--- src/osshs/protocol/interfaces/can/can_frame.cpp
+++ src/osshs/protocol/interfaces/can/can_frame.cpp
@@ -32,20 +32,84 @@ namespace osshs
 		{
 			namespace can
 			{
-				CanFrame::CanFrame(std::unique_ptr<const uint8_t[]> data, uint32_t transmitterMac, uint16_t frameCount = 1, uint16_t frameId = 0, bool error = false)
+				CanFrame::CanFrame(const uint8_t *data, uint8_t dataLen, uint16_t transmitterMac,
+						uint16_t lastFrameId = 0, uint16_t frameId = 0, bool error = false)
 				{
-					this->data = std::move(data);
+					if (lastFrameId == 0)
+					{
+						uint8_t *buffer = new uint8_t[dataLen];
+						std::copy(&data[0], &data[dataLen], &buffer[0]);
+
+						this->data = std::unique_ptr<const uint8_t[]>(buffer);
+						this->dataLen = dataLen;
+					}
+					else
+					{
+						uint8_t *buffer = new uint8_t[dataLen + 1];
+						buffer[0] = frameId > 0 ? frameId & 0x0ff : lastFrameId & 0x0ff;
+						std::copy(&data[0], &data[dataLen], &buffer[1]);
+						
+						this->data = std::unique_ptr<const uint8_t[]>(buffer);
+						this->dataLen = dataLen + 1;
+					}
 
 					extendedIdentifier = 0x00000000;
 					extendedIdentifier |= (static_cast<uint8_t>(!error)) << 28; // NOT_ERROR_FLAG
 					extendedIdentifier |= (static_cast<uint8_t>(frameId == 0)) << 27; // START_FRAME_FLAG
-					extendedIdentifier |= (static_cast<uint8_t>(frameCount > 1)) << 26; // MULTI_FRAME_FLAG
-					extendedIdentifier |= (frameId > 0 ? frameId & 0xf00 : frameCount & 0xf00) << 8; // FRAME_COUNT / FRAME_ID
-					extendedIdentifier |= (transmitterMac & 0x0000ffff) << 0; // TRANSMITTER_MAC
+					extendedIdentifier |= (static_cast<uint8_t>(lastFrameId > 0)) << 26; // MULTI_FRAME_FLAG
+					extendedIdentifier |= (frameId > 0 ? frameId & 0xf00 : lastFrameId & 0xf00) << 16; // FRAME_COUNT / FRAME_ID
+					extendedIdentifier |= transmitterMac; // TRANSMITTER_MAC
+				}
+
+				CanFrame::CanFrame(modm::can::Message message)
+				{
+					data = std::unique_ptr<const uint8_t[]>(message.data);
+					dataLen = message.getLength();
+					extendedIdentifier = message.getIdentifier();
+				}
+
+				const uint8_t *
+				CanFrame::getData()
+				{
+					if (isMultiFrame())
+						return &data[1];
+
+					return &data[0];
+				}
+
+				uint8_t
+				CanFrame::getDataLen()
+				{
+					if (isMultiFrame())
+						return dataLen - 1;
+
+					return dataLen;
+				}
+
+				uint16_t
+				CanFrame::getTransmitterMac()
+				{
+					return extendedIdentifier & 0xffff;
+				}
+
+				uint16_t
+				CanFrame::getFrameId()
+				{
+					return ((extendedIdentifier >> 8) & 0x0f00) | data[0];
+				}
+
+				bool
+				CanFrame::isError()
+				{
+					return (extendedIdentifier >> 28) & 0b1 == 0;
+				}
+
+				bool
+				CanFrame::isMultiFrame()
+				{
+					return (extendedIdentifier >> 26) & 0b1 == 1;
 				}
 			}
 		}
 	}
 }
-
-#endif  // OSSHS_PROTOCOL_CAN_FRAME_HPP
